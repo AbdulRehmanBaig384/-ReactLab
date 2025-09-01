3f04bc3e6b87a4935689a51b7dc0ee1569e8b8a7
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-09-02 01:04:00 +0500

    Update Oops.ts

--- type/Oops.ts
+++ type/Oops.ts
@@ -22,3 +22,34 @@ const account = new BankAccount(1000);
 account.deposit(500);
 
 console.log("Balance:", account.getBalance()); //1500
+
+
+
+// Abstraction in TypeScript
+
+abstract class Shape {
+  
+  abstract area(): number; 
+}
+
+class Circle extends Shape {
+  
+  constructor(private radius: number) {
+    
+    super();
+  }
+
+  area(): number {
+    
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+
+
+const circle = new Circle(5);
+
+console.log("Circle Area:", circle.area()); // 78.5
+
+
+
